9a33f89a89e6d86414f81cd4c1ab09705a023754
ElGiaf <r.faigle29@gmail.com>
2024-06-06 13:29:15 +0200

    .

--- musik/__init__.py
+++ musik/__init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://musikdbuser:vadget-wyhhuz-3cAkry@192.168.0.112/musikdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = "3ecb35af150463b6b420a478902cb4787648611af6e75e9b3d22c1635faae3aa"
+app.config['SECRET_KEY'] = "test"
 
 db = SQLAlchemy(app)
 
